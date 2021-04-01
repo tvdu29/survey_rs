fce942951dde33b430b32b9b2550f3e32b30f83a
Tanguy Mazé <tmaze@student.42.fr>
2021-04-01 15:46:18 +0200

    functionalized prompt

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,121 @@
-use std::io::{Stdout, Write, stdin, stdout};
-use termion::{color, cursor::{self, DetectCursorPos}, event::Key, input::TermRead, raw::{IntoRawMode, RawTerminal}, terminal_size};
+use std::{convert::TryInto, error::Error, io::{stdin, stdout, Stdout, Write}};
+use termion::{
+    color,
+    cursor::{self, DetectCursorPos},
+    event::Key,
+    input::TermRead,
+    raw::{IntoRawMode, RawTerminal},
+    terminal_size,
+};
 
 extern crate termion;
 
-fn move_up(stdout: &mut RawTerminal<Stdout>) {
+fn ask_one<'a>(message: &'a str, options: &'a mut Vec<&str>) -> Result<&'a Vec<&'a str>, Box<dyn Error>> {
+    let stdin = stdin();
+    let mut stdout = stdout().into_raw_mode()?;
+
+    let mut answers: Vec<(bool, &str)> = options
+        .clone()
+        .into_iter()
+        .map(|x| (false, x))
+        .collect();
+
+    write!(
+        stdout,
+        "{}{}",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1)
+    )?;
+    write!(
+        stdout,
+        "{}? {}{}\n\r",
+        color::LightYellow.fg_str(),
+        termion::style::Reset,
+        message
+    )?;
+    let (_, pad) = stdout.cursor_pos()?;
+    for line in &answers {
+        write!(
+            stdout,
+            "{}[{}] {}{}\n\r",
+            if line.0 {
+                color::LightGreen.fg_str()
+            } else {
+                color::LightBlue.fg_str()
+            },
+            if line.0 { 'x' } else { ' ' },
+            termion::style::Reset,
+            line.1
+        )?;
+    }
+
+    write!(stdout, "{}", termion::cursor::Goto(2, (pad).try_into()?))?;
+    stdout.flush()?;
+
+    for c in stdin.keys() {
+        match c? {
+            Key::Char('q') => break,
+            Key::Up => move_up(&mut stdout, &pad),
+            Key::Down => move_down(&mut stdout, &answers, &pad),
+            Key::Char('\n') => check(&mut stdout, &mut answers, &pad),
+            _ => continue,
+        };
+        stdout.flush()?;
+    }
+    write!(stdout, "{}{}", termion::clear::All, cursor::Goto(1, 1))?;
+    stdout.flush()?;
+
+    stdout.suspend_raw_mode()?;
+
+    options.retain(|&x| answers.contains(&(true, x)));
+    Ok(options)
+}
+
+fn move_up(stdout: &mut RawTerminal<Stdout>, pad: &u16) {
     let cursor = stdout.cursor_pos().unwrap();
-    if cursor.1 > 1 {
+    if cursor.1 > *pad {
         write!(stdout, "{}", cursor::Goto(2, cursor.1 - 1)).unwrap();
     };
 }
 
-fn move_down(stdout: &mut RawTerminal<Stdout>, data: &Vec<(bool, &str)>) {
+fn move_down(stdout: &mut RawTerminal<Stdout>, data: &Vec<(bool, &str)>, pad: &u16) {
     let size = terminal_size().unwrap();
     let cursor = stdout.cursor_pos().unwrap();
-    if (cursor.1 as usize) < data.len() && cursor.1 < size.1 {
+    if cursor.1 - pad + 1 < data.len() as u16 && cursor.1 < size.1 {
         write!(stdout, "{}", cursor::Goto(2, cursor.1 + 1)).unwrap();
     };
 }
 
-fn check(stdout: &mut RawTerminal<Stdout>, answers: &mut Vec<(bool, &str)>) {
+fn check(stdout: &mut RawTerminal<Stdout>, answers: &mut Vec<(bool, &str)>, pad: &u16) {
     let cursor = stdout.cursor_pos().unwrap();
-    answers[(cursor.1 - 1) as usize].0 = !answers[(cursor.1 - 1) as usize].0;
-    write!(stdout, "{}{}[{}] {}{}{}", cursor::Goto(1, cursor.1), if answers[(cursor.1 - 1) as usize].0 {color::LightGreen.fg_str()} else {color::LightBlue.fg_str()}, if answers[(cursor.1 - 1) as usize].0 {'x'} else {' '}, termion::style::Reset, answers[(cursor.1 - 1) as usize].1, cursor::Goto(2, cursor.1)).unwrap();
+    answers[(cursor.1 - pad) as usize].0 = !answers[(cursor.1 - pad) as usize].0;
+    write!(
+        stdout,
+        "{}{}[{}] {}{}{}",
+        cursor::Goto(1, cursor.1),
+        if answers[(cursor.1 - pad) as usize].0 {
+            color::LightGreen.fg_str()
+        } else {
+            color::LightBlue.fg_str()
+        },
+        if answers[(cursor.1 - pad) as usize].0 {
+            'x'
+        } else {
+            ' '
+        },
+        termion::style::Reset,
+        answers[(cursor.1 - pad) as usize].1,
+        cursor::Goto(2, cursor.1)
+    )
+    .unwrap();
 }
 
 fn main() {
-    let stdin = stdin();
-    let mut stdout = stdout().into_raw_mode().unwrap();
+    let mut data = vec!["This is line 1", "This is line 2", "This is line 3"];
 
-    let data = vec!["This is line 1", "This is line 2", "This is line 3"];
+    let answers = ask_one("Which line ?", &mut data).unwrap();
 
-    let mut answers: Vec<(bool, &str)> = data.clone().into_iter().map(|x| (false, x)).collect();
-
-    write!(stdout, "{}{}", termion::clear::All, termion::cursor::Goto(1,1)).unwrap();
-    for line in &answers{
-        write!(stdout, "{}[{}] {}{}\n\r", if line.0 {color::LightGreen.fg_str()} else {color::LightBlue.fg_str()}, if line.0 {'x'} else {' '}, termion::style::Reset, line.1).unwrap();
-    };
-
-    write!(stdout, "{}", termion::cursor::Goto(2,1)).unwrap();
-    stdout.flush().unwrap();
-
-    for c in stdin.keys() {
-        match c.unwrap() {
-            Key::Char('q') => break,
-            Key::Up => move_up(&mut stdout),
-            Key::Down => move_down(&mut stdout, &answers),
-            Key::Char('\n') => check(&mut stdout, &mut answers),
-            _ => continue,
-        };
-        stdout.flush().unwrap();
-    }
-    write!(stdout, "{}{}", termion::clear::All, cursor::Goto(1,1)).unwrap();
-    stdout.flush().unwrap();
-
-    stdout.suspend_raw_mode().unwrap();
-
-    answers.retain(|&x| x.0);
     answers.into_iter().for_each(|elem| {
-        println!("{}", elem.1);
+        println!("{}", elem);
     });
 }
